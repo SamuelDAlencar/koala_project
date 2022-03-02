b7e6974162afade40a81e7615bcc39ddbc1592a7
SamuelDAlencar <sarl.2001@hotmail.com>
2022-03-02 08:54:14 -0300

    chore: files setup

---
+++ src/tests/Login.test.js
@@ -0,0 +1,17 @@
+describe('Login', () => {
+  it('Tests if the first page is the login page', () => {
+    
+  });
+  it('', () => {
+    
+  });
+  it('', () => {
+    
+  });
+  it('', () => {
+    
+  });
+  it('', () => {
+    
+  });
+});
