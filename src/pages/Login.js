418dd5b9741a81dac457ef63bf22bd31c5731117
SamuelDAlencar <sarl.2001@hotmail.com>
2022-03-03 22:32:56 -0300

    style: login page style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginAction } from '../redux/actions';
 import { MIN_PASS_LENGTH } from '../consts';
+import '../css/Login.css';
 
 function Login() {
   const navigate = useNavigate();
@@ -46,53 +47,63 @@ function Login() {
 
   return (
     <>
-      <h1>Login</h1>
-      <form>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="wave-top"><path fill="#582d58" fill-opacity="1" d="M0,224L40,197.3C80,171,160,117,240,128C320,139,400,213,480,234.7C560,256,640,224,720,176C800,128,880,64,960,58.7C1040,53,1120,107,1200,138.7C1280,171,1360,181,1400,186.7L1440,192L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z" ></path></svg>
+      <h1 className="login-h1">Log-In</h1>
+      <form className='login-form'>
         <label htmlFor="user-email">
-          Email
+          Email:
+          {' '}
           <input
             id="email"
+            className="login-form-input"
             data-testid="email-input"
             onChange={ inputHandler }
           />
         </label>
-        <label htmlFor="password">
-          Password
+        <section className="password-section">
+          <label htmlFor="password">
+            Password:
+            {' '}
+            <input
+              id="password"
+              className="login-form-input"
+              data-testid="password-input"
+              type={
+                passVisibility
+                ? 'text'
+                : 'password'
+              }
+              onChange={ inputHandler }
+            />
+          </label>
           <input
-            id="password"
-            data-testid="password-input"
-            type={
-              passVisibility
-              ? 'text'
-              : 'password'
-            }
-            onChange={ inputHandler }
-          />
-        </label>
-        <input
-          type="checkbox"
-          onClick={
-            () => setPassVisibility((prevState) => !prevState) } />
+            type="checkbox"
+            className="password-visibility"
+            onClick={
+              () => setPassVisibility((prevState) => !prevState) } />
+          {invalidFields.inexistentUser
+            && <p style={ { color: 'red' } }>
+                There's nobody with this email in our database
+              </p>}
+            {invalidFields.wrongPassword
+            && <p style={ { color: 'red' } }>
+                Wrong password
+              </p>}
+        </section>
         <button
           onClick={ logButton }
           type="button"
+          className="login-form-button"
           disabled={
             !(user.email.includes('@')
             && user.email.includes('.com')
             && user.password.length >= MIN_PASS_LENGTH)
           }
         >Log In</button>
-        <button onClick={ () => navigate('/createaccount') }>
+        <button onClick={ () => navigate('/createaccount') }
+        className="login-form-button">
           Create account
         </button>
-        {invalidFields.inexistentUser
-        && <p style={ { color: 'red' } }>
-            There's nobody with this email in our database
-          </p>}
-        {invalidFields.wrongPassword
-        && <p style={ { color: 'red' } }>
-            Wrong password
-          </p>}
       </form>
     </>
   );
