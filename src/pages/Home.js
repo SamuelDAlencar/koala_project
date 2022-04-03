56ce9940df5d815e5cb1b5392e38c3472537f1de
SamuelDAlencar <sarl.2001@hotmail.com>
2022-04-02 21:31:31 -0300

    style: added a login and create account option to the home header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { fetchProducts } from "../redux/actions";
@@ -25,21 +25,10 @@ function Home() {
       width: "100%",
       height: "80%",
     },
-    input: {
-      width: "70%",
-      color: "#EEEEEE",
-      fontSize: "140%",
-    },
-    buttons: {
-      width: "30%",
-      fontSize: "100%",
-    },
-    searchContainer: {
-      marginTop: "110px",
-      display: "flex",
-      width: "100%",
-    },
     cards: {
+    },
+    homeContainer: {
+      marginTop: "80px",
     }
   })
   const classes = useStyles();
@@ -54,50 +43,43 @@ function Home() {
 
   const dispatch = useDispatch();
   const store = useSelector((storeData) => ({
-    products: storeData.products.results
+    products: storeData.products.results,
+    toggleHome: storeData.toggleHome,
   }));
-  const [inputValue, setInputValue] = useState();
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
 
-  const inputHandler = ({ target: { value }}) => {
-    setInputValue(value);
-  }
-
-  const searchButton = () => {
-    dispatch(fetchProducts(inputValue));
-  } 
-
   return (
     <ThemeProvider theme={ theme }>
       <Header />
-      <Container>
-        <Grid
-          container
-          spacing={6}
-        >
-          {store.products
-            && store.products.map((product) => 
-            <Grid
-              xs={12}
-              sm={6}
-              md={4}
-              lg={3}
-              item
-              key={product.id}
-            >
-              <Box
-                className={classes.card}
+      {store.toggleHome &&
+        <Container className={classes.homeContainer}>
+          <Grid
+            container
+            spacing={6}
+          >
+            {store.products
+              && store.products.map((product) => 
+              <Grid
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                item
+                key={product.id}
               >
-                <img className={classes.media} alt={product.title} src={product.thumbnail} />
-                <h2>{product.title}</h2>
-              </Box>
-            </Grid>
-          )}
-        </Grid>
-      </Container>
+                <Box
+                  className={classes.card}
+                >
+                  <img className={classes.media} alt={product.title} src={product.thumbnail} />
+                  <h2>{product.title}</h2>
+                </Box>
+              </Grid>
+            )}
+          </Grid>
+        </Container>}
       {/* <Footer /> */}
     </ThemeProvider>
   );
